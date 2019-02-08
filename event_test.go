7cfbed3385ca45eaf44e78419b995a0e1c30c892
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-08 10:55:30 +0100

    Refactor the event collection

--- event_test.go
+++ event_test.go
@@ -6,23 +6,23 @@ import (
 	"time"
 )
 
-func Examplebetween() {
+func ExampleBetween() {
 	before := time.Now()
 	A := time.Now()
 	t := time.Now()
 	B := time.Now()
 	after := time.Now()
 
-	fmt.Println(between(t, A, B))          // true
-	fmt.Println(between(t, before, A))     // false
-	fmt.Println(between(t, B, after))      // false
-	fmt.Println(between(t, before, after)) // true
-	fmt.Println(between(t, before, B))     // true
-	fmt.Println(between(t, A, after))      // true
-	fmt.Println(between(t, after, before)) // false
+	fmt.Println(Between(t, A, B))          // true
+	fmt.Println(Between(t, before, A))     // false
+	fmt.Println(Between(t, B, after))      // false
+	fmt.Println(Between(t, before, after)) // true
+	fmt.Println(Between(t, before, B))     // true
+	fmt.Println(Between(t, A, after))      // true
+	fmt.Println(Between(t, after, before)) // false
 
-	fmt.Println(between(t, t, B)) // true (from inclusive, to exclusive)
-	fmt.Println(between(t, A, t)) // false (from inclusive, to exclusive)
+	fmt.Println(Between(t, t, B)) // true (from inclusive, to exclusive)
+	fmt.Println(Between(t, A, t)) // false (from inclusive, to exclusive)
 
 	// Output:
 	// true
@@ -36,7 +36,9 @@ func Examplebetween() {
 	// false
 }
 
-func addEvents(s *EventLoop) {
+func createEvents() *Events {
+	events := NewEvents()
+
 	in0sec := time.Now()
 
 	in2sec := in0sec.Add(2 * time.Second)
@@ -44,43 +46,29 @@ func addEvents(s *EventLoop) {
 	in15sec := in0sec.Add(15 * time.Second)
 	in22sec := in0sec.Add(22 * time.Second)
 
-	// Create two new events
-	s.AddEvent(NewEvent(in5sec, 1*time.Second, 5*time.Second, func() {
+	// Create a couple of new events
+	events.Add(New(in5sec, 5*time.Second, 1*time.Second, func() {
 		fmt.Println("This happens after 5s, cooldown: 1s, window: 5s")
 	}))
-	s.AddEvent(NewEvent(in15sec, 2*time.Second, 2*time.Second, func() {
+	events.Add(New(in15sec, 2*time.Second, 2*time.Second, func() {
 		fmt.Println("This happens once after 15s")
 	}))
-	s.AddEvent(NewEvent(in2sec, 3*time.Second, 30*time.Second, func() {
+	events.Add(New(in2sec, 30*time.Second, 3*time.Second, func() {
 		fmt.Println("This happens every 3s, within a 30 second window")
 	}))
-	s.AddEvent(NewEvent(in0sec, 2*time.Second, 20*time.Second, ProgressWrapper(in2sec, in22sec, func(p float64) {
+	events.Add(New(in0sec, 20*time.Second, 2*time.Second, ProgressWrapperInterval(in2sec, in22sec, 2*time.Second, func(p float64) {
 		fmt.Printf("This happens every 2s, within a 20s window: %d%% complete\n", int(p*100))
 	})))
 
+	return events
 }
 
-//func addTransitions() {
-//	in10sec := time.Now().Add(10 * time.Second)
-//	in20sec := time.Now().Add(20 * time.Second)
-//
-//	// Create two new events
-//	AddReEvent(NewTransition(in10sec, 2*time.Second, func(progress float64) {
-//		fmt.Println("This event happens after 10 seconds, within a 2 second window! Progress:", progress)
-//	}))
-//	AddReEvent(NewTransition(in20sec, 2*time.Second, func(progress float64) {
-//		fmt.Println("This event happens after 20 seconds, within a 2 second window! Progress:", progress)
-//	}))
-//}
-
 func TestEventLoop(t *testing.T) {
-	s := NewEventLoop()
-	addEvents(s)
-	//addTransitions()
+	events := createEvents()
 
 	// TODO: Find the smallest cooldown and use that as the event loop delay
 	fmt.Println("Run the event loop for 40s, with a loop delay of 50ms")
-	go s.Go(50 * time.Millisecond)
+	go events.Loop(50 * time.Millisecond)
 
 	time.Sleep(40 * time.Second)
 }
