b4aa109ced5c585172a73b7af77c11dd3b70610c
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-08 11:21:19 +0100

    Add the Once and OnceWindow functions

--- event_test.go
+++ event_test.go
@@ -46,19 +46,28 @@ func createEvents() *Events {
 	in15sec := in0sec.Add(15 * time.Second)
 	in22sec := in0sec.Add(22 * time.Second)
 
-	// Create a couple of new events
+	// Create four new events
 	events.Add(New(in5sec, 5*time.Second, 1*time.Second, func() {
-		fmt.Println("This happens after 5s, cooldown: 1s, window: 5s")
+		fmt.Println("A happens after 5s, cooldown: 1s, window: 5s")
 	}))
 	events.Add(New(in15sec, 2*time.Second, 2*time.Second, func() {
-		fmt.Println("This happens once after 15s")
+		fmt.Println("B happens once after 15s")
 	}))
 	events.Add(New(in2sec, 30*time.Second, 3*time.Second, func() {
-		fmt.Println("This happens every 3s, within a 30 second window")
+		fmt.Println("C happens every 3s, within a 30 second window")
 	}))
 	events.Add(New(in0sec, 20*time.Second, 2*time.Second, ProgressWrapperInterval(in2sec, in22sec, 2*time.Second, func(p float64) {
-		fmt.Printf("This happens every 2s, within a 20s window: %d%% complete\n", int(p*100))
+		fmt.Printf("D happens every 2s, within a 20s window: %d%% complete\n", int(p*100))
 	})))
+
+	// Add one-time events for the time markers
+	for i := 0; i < 40; i += 10 {
+		// Needed for the integer i to be enclosed correctly in the closure below
+		seconds := i
+		events.Once(in0sec.Add(time.Duration(seconds)*time.Second), func() {
+			fmt.Printf("--- %d second mark ---\n", seconds)
+		})
+	}
 
 	return events
 }
@@ -67,8 +76,9 @@ func TestEventLoop(t *testing.T) {
 	events := createEvents()
 
 	// TODO: Find the smallest cooldown and use that as the event loop delay
-	fmt.Println("Run the event loop for 40s, with a loop delay of 50ms")
+	fmt.Println("Run the event loop for 30s, with a loop delay of 50ms")
 	go events.Loop(50 * time.Millisecond)
 
-	time.Sleep(40 * time.Second)
+	// Wait 30 seconds before returning
+	time.Sleep(30 * time.Second)
 }
